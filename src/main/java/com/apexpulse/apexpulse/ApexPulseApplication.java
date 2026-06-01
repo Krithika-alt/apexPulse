2e569eb24afd9211da78db4714df3684468d91d5
Krithika-alt <krithika12907@gmail.com>
2026-06-01 13:27:59 +1000

    Initial Project Setup

---
+++ src/main/java/com/apexpulse/apexpulse/ApexPulseApplication.java
@@ -0,0 +1,13 @@
+package com.apexpulse.apexpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApexPulseApplication.class, args);
+    }
+
+}
